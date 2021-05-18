55ffe85368b53a0e3421ec7251c12224ce1e1116
Abelbimaw <abelwiratama@gmail.com>
2021-05-18 16:57:58 +0700

    Learn about Filesystem asynchronous anda asynchronous

---
+++ Filesystem/tes.js
@@ -0,0 +1,18 @@
+//Asynchronous
+const fs = require('fs');
+
+const fileReadCallback = (error, data) => {
+    if(error){
+        console.log('Gagal membaca berkas');
+        return;
+    }
+    console.log(data);
+};
+
+fs.readFile('todo.txt', 'UTF-8', fileReadCallback);
+
+//Synchronous
+// const fs = require('fs');
+ 
+// const data = fs.readFileSync('todo.txt', 'UTF-8');
+// console.log(data);
