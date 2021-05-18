b181beb59195c68a39704ce714bd97b1d8918886
Abelbimaw <abelwiratama@gmail.com>
2021-05-18 16:27:37 +0700

    Membuat event listener lebih dari 1 menggunakan fungsi on

--- Event/tes.js
+++ Event/tes.js
@@ -1,11 +1,17 @@
-const {EventEmitter} = require('ecent');
+const {EventEmitter} = require('events');
 
 const myEventEmitter = new EventEmitter();
 
 //Fungsi yang akan dijalankan ketika evnet coffee-order terjadi
-const makecoffee = ({ name }) => {
+const makeCoffee = ({ name }) => {
     console.log(`Kopi ${name} telah dibuat !`);
 };
 
+const makeBill = ({ price }) => {
+    console.log(`Bill anda sebesar ${price} telah dibuat!`);
+}
 //mendaftarkan fungsi makecoffe sebagai listener event coffee-order
-myEventEmitter.on('coffee-order', makecoffee);
+myEventEmitter.on('coffee-order', makeCoffee);
+myEventEmitter.on('coffee-order', makeBill);
+
+myEventEmitter.emit('coffee-order', {name : 'Tubruk', price : 15000});
